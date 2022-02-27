0f40598cfc5385a90f3d10384a6bf72e34f54911
Thiago Gois <thiagogoisjunior@gmail.com>
2022-02-27 10:12:17 -0300

    Modal Open and Close ✅

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,19 +1,26 @@
-import { Modal } from './modal'
+import Modal from './modal.js'
 
 const modal = Modal()
+
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
 
 //PEGAR TODOS OS BOTÕES QUE EXISTE COM A CLASSE CHECK
 const checkButtons = document.querySelectorAll(".actions a.check")
 
 checkButtons.forEach(button => {
     //adicionar a escuta
-    button.eventListener('click',event => {
+    button.addEventListener('click',event => {
         modal.open()
     })
 })
 
-//PEGAR QUANDO O "MARCAR COMO LIDO" FOR CLICADO
 
+const deleteButton = document.querySelectorAll('.actions a.delete')
 
-// ABRIR MODAL
-modal.open()
+deleteButton.forEach(button => {
+    button.addEventListener('click', event => {
+        modal.open()
+    })
+})
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,10 +1,24 @@
-module.exports = function Modal(){
+export default function Modal(){
+
+
+    const modalWrapper = document.querySelector('.modal-wrapper')
+    const cancelButton = document.querySelector('.button.cancel')
+
+    cancelButton.addEventListener('click', close)
     
     // FUNCÇÃO DE ABRIR MODAL
-    function open(){}
+    function open(){
+
+        document.querySelector('.modal-wrapper').classList.add('active');
+
+    }
 
     // FUNÇÃO DE FECHAR A MODAL
-    function close(){}
+    function close(){
+
+        modalWrapper.classList.remove('active')
+
+    }
 
     return {
         open,
